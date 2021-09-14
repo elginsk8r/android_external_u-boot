f9befc50d6ca9bc74ba4822106431fdd3cfb735d
Nick Reuter <nreuter85@gmail.com>
2021-09-13 19:14:39 -0500

    board: meson: add odroid-n2 android support
    Add all the necessary bits to flash and boot Android for
    Hardkernel Odroid-N2 boards.
    
    Change-Id: I5dedcaedc35e1a819b2f87c29198074279502907

---
+++ include/configs/odroid-n2_android.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Configuration for the khadas VIM3 Android
+ *
+ * Copyright (C) 2021 Baylibre, SAS
+ * Author: Guillaume LA ROQUE <glaroque@baylibre.com>
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#define EXTRA_ANDROID_ENV_SETTINGS \
+	"board=odroid-n2\0" \
+	"board_name=odroid-n2\0" \
+
+#include <configs/meson64_android.h>
+
+#endif /* __CONFIG_H */
