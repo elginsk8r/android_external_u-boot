83dc8c5f42337c84c1e71240bed633ba373c95f8
Nick Reuter <nreuter85@gmail.com>
2021-09-20 18:39:03 -0500

    vim3(l)/odroid-n2: add 512M to the super partition
    Change-Id: I2652d2e7238ca78b5bde3bac8a9a263a7fc04904

--- include/configs/odroid-n2_android.h
+++ include/configs/odroid-n2_android.h
@@ -23,8 +23,8 @@
 	"name=vbmeta_b,size=512K,uuid=${uuid_gpt_vbmeta_b};" \
 	"name=boot_a,size=32M,bootable,uuid=${uuid_gpt_boot_a};" \
 	"name=boot_b,size=32M,bootable,uuid=${uuid_gpt_boot_b};" \
-	"name=super,size=3072M,uuid=${uuid_gpt_super};"	\
-	"name=userdata,size=11282M,uuid=${uuid_gpt_userdata};" \
+	"name=super,size=4608M,uuid=${uuid_gpt_super};"	\
+	"name=userdata,size=10217M,uuid=${uuid_gpt_userdata};" \
 	"name=rootfs,size=-,uuid=" ROOT_UUID
 
 #else
@@ -37,8 +37,8 @@
 	"name=boot,size=32M,bootable,uuid=${uuid_gpt_boot};" \
 	"name=recovery,size=32M,uuid=${uuid_gpt_recovery};"	\
 	"name=cache,size=256M,uuid=${uuid_gpt_cache};"	\
-	"name=super,size=1792M,uuid=${uuid_gpt_super};"	\
-	"name=userdata,size=12786M,uuid=${uuid_gpt_userdata};" \
+	"name=super,size=2304M,uuid=${uuid_gpt_super};"	\
+	"name=userdata,size=12274M,uuid=${uuid_gpt_userdata};" \
 	"name=rootfs,size=-,uuid=" ROOT_UUID
 #endif
 
