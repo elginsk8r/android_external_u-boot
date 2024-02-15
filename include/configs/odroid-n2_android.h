665a7e2960d5ba7a30c5310009dee62f95786b76
Guillaume La Roque <glaroque@baylibre.com>
2024-02-15 18:29:00 +0100

    include: configs: khadas-vim3|vim3l|odroid-n2: update partition table
    - add new partition frp and metadata.
    
    Change-Id: I8d93c45108ea6a85652b140cd11cd994e47d49ce
    Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

--- include/configs/odroid-n2_android.h
+++ include/configs/odroid-n2_android.h
@@ -10,13 +10,13 @@
 #define __CONFIG_H
 
 #define LOGO_UUID "43a3305d-150f-4cc9-bd3b-38fca8693846;"
-#define ROOT_UUID "ddb8c3f6-d94d-4394-b633-3134139cc2e0;"
 
 #if defined(CONFIG_CMD_BCB) && defined(CONFIG_ANDROID_AB)
 #define PARTS_DEFAULT \
 	"uuid_disk=${uuid_gpt_disk};" \
 	"name=logo,start=512K,size=2M,uuid=" LOGO_UUID \
 	"name=misc,size=512K,uuid=${uuid_gpt_misc};" \
+	"name=frp,size=512K,uuid=${uuid_gpt_frp};" \
 	"name=dtbo_a,size=8M,uuid=${uuid_gpt_dtbo_a};" \
 	"name=dtbo_b,size=8M,uuid=${uuid_gpt_dtbo_b};" \
 	"name=vbmeta_a,size=512K,uuid=${uuid_gpt_vbmeta_a};" \
@@ -24,21 +24,22 @@
 	"name=boot_a,size=64M,bootable,uuid=${uuid_gpt_boot_a};" \
 	"name=boot_b,size=64M,bootable,uuid=${uuid_gpt_boot_b};" \
 	"name=super,size=3072M,uuid=${uuid_gpt_super};" \
-	"name=userdata,size=11218M,uuid=${uuid_gpt_userdata};" \
-	"name=rootfs,size=-,uuid=" ROOT_UUID
+	"name=metadata,size=16M,bootable,uuid=${uuid_gpt_metadata};" \
+	"name=userdata,size=-,uuid=${uuid_gpt_userdata};"
 #else
 #define PARTS_DEFAULT \
 	"uuid_disk=${uuid_gpt_disk};" \
 	"name=logo,start=512K,size=2M,uuid=" LOGO_UUID \
 	"name=misc,size=512K,uuid=${uuid_gpt_misc};" \
+	"name=frp,size=512K,uuid=${uuid_gpt_frp};" \
 	"name=dtbo,size=8M,uuid=${uuid_gpt_dtbo};" \
 	"name=vbmeta,size=512K,uuid=${uuid_gpt_vbmeta};" \
 	"name=boot,size=64M,bootable,uuid=${uuid_gpt_boot};" \
 	"name=recovery,size=64M,uuid=${uuid_gpt_recovery};" \
 	"name=cache,size=256M,uuid=${uuid_gpt_cache};" \
 	"name=super,size=1792M,uuid=${uuid_gpt_super};" \
-	"name=userdata,size=12722M,uuid=${uuid_gpt_userdata};" \
-	"name=rootfs,size=-,uuid=" ROOT_UUID
+	"name=metadata,size=16M,bootable,uuid=${uuid_gpt_metadata};" \
+	"name=userdata,size=-,uuid=${uuid_gpt_userdata};"
 #endif
 
 #define CFG_EXTRA_ENV_SETTINGS                                    \
