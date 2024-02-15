63bf48968e3b886299c312d33b0288567a68cc27
Guillaume La Roque <glaroque@baylibre.com>
2024-02-15 18:29:00 +0100

    include: configs: meson64_android: Add frp and metadata partitions
    Change-Id: I8d93c45108ea6a85652b140cd11cd994e47d49ce
    Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

--- include/configs/sei510.h
+++ include/configs/sei510.h
@@ -15,12 +15,14 @@
 	"uuid_disk=${uuid_gpt_disk};" \
 	"name=logo,size=2M,uuid=" LOGO_UUID \
 	"name=misc,size=128K,uuid=${uuid_gpt_misc};" \
+	"name=frp,size=512K,uuid=${uuid_gpt_frp};" \
 	"name=dtbo,size=8M,uuid=${uuid_gpt_dtbo};" \
 	"name=vbmeta,size=64K,uuid=${uuid_gpt_vbmeta};" \
 	"name=boot,size=64M,bootable,uuid=${uuid_gpt_boot};" \
 	"name=recovery,size=64M,uuid=${uuid_gpt_recovery};" \
 	"name=cache,size=256M,uuid=${uuid_gpt_cache};" \
 	"name=super,size=1792M,uuid=${uuid_gpt_super};" \
+	"name=metadata,size=16M,bootable,uuid=${uuid_gpt_metadata};" \
 	"name=userdata,size=-,uuid=${uuid_gpt_userdata};"
 
 #include <configs/meson64_android.h>
